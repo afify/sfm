0a71c22d50ba743222736da29d97a0aa560d9578
mohamedafify <mohamed@afify.dev>
2020-07-14 16:18:02 +0200

    [refactor] adjusting const variables

--- config.def.h
+++ config.def.h
@@ -7,26 +7,26 @@
 
 /* colors */
 static const uint16_t dir_b     = TB_DEFAULT;
-static const uint16_t dir_f     = (uint16_t)33;
+static const uint16_t dir_f     = 33;
 static const uint16_t file_b    = TB_DEFAULT;
 static const uint16_t file_f    = TB_DEFAULT;
 static const uint16_t frame_b   = TB_DEFAULT;
 static const uint16_t frame_f   = TB_DEFAULT;
 static const uint16_t other_b   = TB_DEFAULT;
-static const uint16_t other_f   = (uint16_t)3;
+static const uint16_t other_f   = 3;
 static const uint16_t pane_l_b  = TB_DEFAULT;
-static const uint16_t pane_l_f  = (uint16_t)166;
+static const uint16_t pane_l_f  = 166;
 static const uint16_t pane_r_b  = TB_DEFAULT;
-static const uint16_t pane_r_f  = (uint16_t)5;
-static const uint16_t search_b  = (uint16_t)166;
-static const uint16_t search_f  = (uint16_t)255;
-static const uint16_t status_b  = (uint16_t)234;
+static const uint16_t pane_r_f  = 5;
+static const uint16_t search_b  = 166;
+static const uint16_t search_f  = 255;
+static const uint16_t status_b  = 234;
 static const uint16_t status_f  = TB_DEFAULT;
-static const uint16_t sprompt_f = (uint16_t)33;
-static const uint16_t sprompt_b = (uint16_t)234;
-static const uint16_t serr_f    = (uint16_t)124;
-static const uint16_t serr_b    = (uint16_t)234;
-static const uint16_t exec_f    = (uint16_t)2;
+static const uint16_t sprompt_f = 33;
+static const uint16_t sprompt_b = 234;
+static const uint16_t serr_f    = 124;
+static const uint16_t serr_b    = 234;
+static const uint16_t exec_f    = 2;
 
 /* bookmarks */
 static Bookmark bmarks[] = {
@@ -39,9 +39,9 @@ static Bookmark bmarks[] = {
 static const char *images[] = { "bmp", "jpg", "jpeg", "png", "gif", "xpm" };
 static const char *web[] = { "htm", "html" };
 static const char *pdf[] = { "epub", "pdf" };
-static const char *videos[] = { "ac3", "avi", "divx", "flc", "fli", "flv",
+static const char *videos[] = { "avi", "flv",
 	"m2v", "m4a", "m4v", "mkv", "mov", "mp3", "mp4", "mpeg", "mpg",
-	"mts", "opus", "qt", "ra", "rm", "ts", "wav", "webm", "wma", "wmv" };
+	"wav", "webm", "wma", "wmv" };
 static const char *documents[] = { "odt", "doc", "docx", "xls", "xlsx", "odp",
 	"ods", "pptx", "odg" };
 static const char *arts[] = { "xcf" };
@@ -64,7 +64,6 @@ static const int show_ug     = 1;
 static const int show_perm   = 1;
 static const int show_dt     = 1;
 static const int show_size   = 1;
-static const int show_count  = 1;
 
 /* unicode chars */
 static const uint32_t u_cne = 0x2510;
