13234890a25c57571fc2cf8beaba607afb2952f5
afify <hassan@afify.dev>
2021-02-27 17:36:28 +0300

    fix pthread

--- config.mk
+++ config.mk
@@ -6,9 +6,9 @@ PREFIX    = /usr/local
 MANPREFIX = ${PREFIX}/share/man
 
 # flags
-CPPFLAGS = -DVERSION=\"$(VERSION)\" -D_DEFAULT_SOURCE -D_XOPEN_SOURCE=700 -D_BSD_SOURCE
+CPPFLAGS = -DVERSION=\"${VERSION}\" -D_DEFAULT_SOURCE -D_XOPEN_SOURCE=700 -D_BSD_SOURCE
 CFLAGS   = -std=c99 -pedantic -Wall -Wextra -Os ${CPPFLAGS}
-LDFLAGS  = -lpthread -s
+LDFLAGS  = -pthread -s
 
 # compiler and linker
 CC = cc
