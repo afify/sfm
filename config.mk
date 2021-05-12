90149dca9132b26dd165a741fe122140ee6b0473
David Kalliecharan <davidkallie@gmail.com>
2021-05-12 16:04:22 -0300

    Autodetect macOS and include the necessary _DARWIN_C_SOURCE

--- config.mk
+++ config.mk
@@ -7,6 +7,11 @@ MANPREFIX = ${PREFIX}/share/man
 
 # flags
 CPPFLAGS = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_POSIX_C_SOURCE=200809L -D_XOPEN_SOURCE=700 -DVERSION=\"${VERSION}\"
+
+ifeq ($(shell uname -s),Darwin)
+CPPFLAGS += -D_DARWIN_C_SOURCE
+endif
+
 CFLAGS   = -std=c99 -pedantic -Wextra -Wall -Os ${CPPFLAGS}
 LDFLAGS  = -s
 
