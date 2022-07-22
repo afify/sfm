5193fbb52562fa0cee482cd77cd634f3f40c8ef4
afify <hassan@afify.dev>
2022-07-22 16:16:24 +0300

    fix config

--- config.def.h
+++ config.def.h
@@ -14,7 +14,7 @@ static const Cpair csock   = { 92,  0,   NORM };
 static const Cpair cexec   = { 91,  0,   NORM };
 static const Cpair cother  = { 90,  0,   NORM };
 
-static const Cpair cframe  = { 233, 5,   NORM };
+static const Cpair cframe  = { 0,   9, NORM };
 static const Cpair cpanell = { 166, 233, BOLD };
 static const Cpair cpanelr = { 5,   233, BOLD };
 static const Cpair cerr    = { 124, 0,   NORM };
@@ -22,10 +22,132 @@ static const Cpair cprompt = { 33,  0,   NORM };
 static const Cpair csearch = { 255, 0,   NORM };
 static const Cpair cstatus = { 243, 233, NORM };
 
-/* statusbar */
-static const char dtfmt[] = "%F %R"; /* date time format */
+/* commands */
+#if defined(__linux__)
+#define CHFLAG "chattr"
+#else
+#define CHFLAG "chflags"
+#endif
+static const char *rm_cmd[]      = { "rm", "-rf" }; /* delete */
+static const char *cp_cmd[]      = { "cp", "-r" }; /* copy */
+static const char *chown_cmd[]   = { "chown", "-R" }; /* change file owner and group */
+static const char *chmod_cmd[]   = { "chmod" }; /* change file mode bits */
+static const char *chflags_cmd[] = { CHFLAG }; /* change file flags */
+static const char *mv_cmd[]      = { "mv" }; /* move */
+static const char delconf[]      = "yes";
+
+static const size_t rm_cmd_len      = LEN(rm_cmd);
+static const size_t cp_cmd_len      = LEN(cp_cmd);
+static const size_t chown_cmd_len   = LEN(chown_cmd);
+static const size_t chmod_cmd_len   = LEN(chmod_cmd);
+static const size_t chflags_cmd_len = LEN(chflags_cmd);
+static const size_t mv_cmd_len      = LEN(mv_cmd);
+static const size_t delconf_len     = LEN(delconf);
+
+///* bookmarks */
+//static const char root[]   = "/";
+//
+///* software */
+//static const char *mpv[]          = { "mpv", "--fullscreen" };
+//static const char *sxiv[]         = { "sxiv" };
+//static const char *mupdf[]        = { "mupdf", "-I" };
+//static const char *libreoffice[]  = { "libreoffice" };
+//static const char *gimp[]         = { "gimp" };
+//static const char *r2[]           = { "r2", "-c", "vv" };
+//
+///* extensions*/
+//static const char *images[]    = { "bmp", "jpg", "jpeg", "png", "gif", "webp", "xpm" };
+//static const char *pdf[]       = { "epub", "pdf" };
+//static const char *arts[]      = { "xcf" };
+//static const char *obj[]       = { "o", "a", "so" };
+//static const char *videos[]    = { "avi", "flv", "wav", "webm", "wma", "wmv",
+//				   "m2v", "m4a", "m4v", "mkv", "mov", "mp3",
+//				   "mp4", "mpeg", "mpg" };
+//static const char *documents[] = { "odt", "doc", "docx", "xls", "xlsx", "odp",
+//				   "ods", "pptx", "odg" };
+//
+//static Rule rules[] = {
+//	{videos,    LEN(videos),    mpv,         LEN(mpv)         },
+//	{images,    LEN(images),    sxiv,        LEN(sxiv)        },
+//	{pdf,       LEN(pdf),       mupdf,       LEN(mupdf)       },
+//	{documents, LEN(documents), libreoffice, LEN(libreoffice) },
+//	{arts,      LEN(arts),      gimp,        LEN(gimp)        },
+//	{obj,       LEN(obj),       r2,          LEN(r2)          },
+//};
+
+/* normal keys */
+static Key nkeys[] = {
+	/* keyval                      function      arg */
+	{ 'j',                 mv_ver,       {.i = -1}       },
+	{ XK_DOWN,             mv_ver,       {.i = -1}       },
+	{ 'k',                 mv_ver,       {.i = +1}       },
+	{ XK_UP,               mv_ver,       {.i = +1}       },
+	{ XK_CTRL('u'),        mv_ver,       {.i = +3}       },
+	{ XK_CTRL('d'),        mv_ver,       {.i = -3}       },
+	//{ {.ch = 'l'},                 mvfwd,        {.i = 0}        },
+	//{ {.key = TB_KEY_ARROW_RIGHT}, mvfwd,        {.i = 0}        },
+	//{ {.ch = 'h'},                 mvbk,         {.i = 0}        },
+	//{ {.key = TB_KEY_ARROW_LEFT},  mvbk,         {.i = 0}        },
+	{ 'g',                 mvtop,        {.i = 0}        },
+	{ 'G',                 mvbtm,        {.i = 0}        },
+	//{ {.ch = 'n'},                 crnf,         {0}             },
+	//{ {.ch = 'N'},                 crnd,         {0}             },
+	//{ {.ch = 'd'},                 delent,       {0}             },
+	//{ {.ch = 'D'},                 dupl,         {0}             },
+	//{ {.ch = 'x'},                 calcdir,      {0}             },
+	//{ {.ch = '/'},                 start_filter, {0}             },
+	{ 'q',                 quit,         {0}             },
+	//{ {.ch = 'v'},                 start_vmode,  {0}             },
+	//{ {.ch = 'y'},                 yank,         {0}             },
+	//{ {.ch = 'p'},                 paste,        {0}             },
+	//{ {.ch = 'P'},                 selmv,        {0}             },
+	//{ {.ch = 'c'},                 start_change, {0}             },
+	//{ {.ch = 'b'},                 opnsh,        {0}             },
+	{ XK_SPACE,       switch_pane,  {0}             },
+	//{ {.key = TB_KEY_CTRL_R},      refresh,      {0}             },
+	//{ {.ch = '\\'},                bkmrk,        {.v = root}     },
+	//{ {.ch = '.'},                 toggle_df,    {0}             },
+};
+
+///* change keys */
+//static Key ckeys[] = {
+//	/* keyval                      function         arg */
+//	{ {.ch = 'w'},                 rname,            {0}            },
+//	{ {.ch = 'o'},                 chngo,            {0}            },
+//	{ {.ch = 'm'},                 chngm,            {0}            },
+//	{ {.ch = 'f'},                 chngf,            {0}            },
+//	{ {.ch = 'q'},                 exit_change,      {0}            },
+//	{ {.ch = 'c'},                 exit_change,      {0}            },
+//	{ {.key = TB_KEY_ESC},         exit_change,      {0}            },
+//};
+//
+///* visual keys */
+//static Key vkeys[] = {
+//	/* keyval                      function         arg */
+//	{ {.ch = 'j'},                 seldwn,          {.i = -1}      },
+//	{ {.key = TB_KEY_ARROW_DOWN},  seldwn,          {.i = -1}      },
+//	{ {.ch = 'k'},                 selup,           {.i = +1}      },
+//	{ {.key = TB_KEY_ARROW_UP},    selup,           {.i = +1}      },
+//	{ {.ch = 'a'},                 selall,          {0}            },
+//	{ {.ch = 'y'},                 selynk,          {0}            },
+//	{ {.ch = 'd'},                 seldel,          {.v = delconf} },
+//	{ {.ch = 'q'},                 exit_vmode,      {0}            },
+//	{ {.ch = 'v'},                 exit_vmode,      {0}            },
+//	{ {.key = TB_KEY_ESC},         exit_vmode,      {0}            },
+//};
+
+static const size_t nkeyslen = LEN(nkeys);
+//static const size_t vkeyslen = LEN(vkeys);
+//static const size_t ckeyslen = LEN(ckeys);
+
+/* permissions */
+static const mode_t ndir_perm = S_IRWXU;
+static const mode_t nf_perm   = S_IRUSR | S_IWUSR;
 
 /* dotfiles */
 static int show_dotfiles = 1;
+
+/* statusbar */
+static const char dtfmt[] = "%F %R"; /* date time format */
 
 #endif /* CONFIG_H */
