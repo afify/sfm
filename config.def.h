a4702eb27e49b0d315a0e17dd8b02cf56a48c5ea
afify <hassan@afify.dev>
2022-08-11 10:47:44 +0300

    fix config file

--- config.def.h
+++ config.def.h
@@ -4,25 +4,28 @@
 #define CONFIG_H
 
 /* colors                      fg,  bg   att*/
-static const Cpair cdir    = { 31,  0,   NORM };
-static const Cpair cfile   = { 243, 0,   NORM };
-static const Cpair clnk    = { 96,  0,   NORM };
-static const Cpair cbrlnk  = { 0,   9,   NORM };
 static const Cpair cblk    = { 95,  0,   NORM };
+static const Cpair cbrlnk  = { 0,   9,   NORM };
 static const Cpair cchr    = { 94,  0,   NORM };
-static const Cpair cifo    = { 93,  0,   NORM };
-static const Cpair csock   = { 92,  0,   NORM };
+static const Cpair cdir    = { 31,  0,   NORM };
+static const Cpair cerr    = { 124, 0,   NORM };
 static const Cpair cexec   = { 91,  0,   NORM };
-static const Cpair cother  = { 90,  0,   NORM };
-
+static const Cpair cfile   = { 243, 0,   NORM };
 static const Cpair cframe  = { 233, 233, NORM };
+static const Cpair cifo    = { 93,  0,   NORM };
+static const Cpair clnk    = { 96,  0,   NORM };
+static const Cpair cother  = { 90,  0,   NORM };
 static const Cpair cpanell = { 166, 233, BOLD };
 static const Cpair cpanelr = { 5,   233, BOLD };
-static const Cpair cerr    = { 124, 0,   NORM };
-static const Cpair cwarn   = { 220, 0,   NORM };
-static const Cpair cprompt = { 33,  98,  NORM };
+static const Cpair cprompt = { 33,  0,   NORM };
 static const Cpair csearch = { 255, 0,   NORM };
+static const Cpair csock   = { 92,  0,   NORM };
 static const Cpair cstatus = { 243, 0,   NORM };
+static const Cpair cwarn   = { 220, 0,   NORM };
+
+/* fallback */
+static char fallback_editor[] = "vi";
+static char fallback_shell[] = "/bin/sh";
 
 /* commands */
 #if defined(__linux__)
